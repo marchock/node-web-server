8b994a6b307b73a9f8471a06622bf0ea2bf59f3b
ryan <ryanmarchock@gmail.com>
2016-11-30 00:14:12 +0000

    update package and server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs')
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -60,6 +62,6 @@ app.get('/about', (req, res) => {
 // })
 
 
-app.listen(3000, () => {
-    console.log('server is running port 3000')
+app.listen(port, () => {
+    console.log(`server is running port ${port}`)
 });
